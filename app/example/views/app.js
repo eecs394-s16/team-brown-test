07829937d0b01e02e606fa2ce595c2e8c760c8a0
Yi Zhang <yi.zhang.abc@gmail.com>
2016-04-07 11:09:32 -0500

    First Prototype

--- app/example/views/app.js
+++ app/example/views/app.js
@@ -1,22 +1,3 @@
-angular.module('example', [
-  // Declare here all AngularJS dependencies that are shared by the example module.
-  'supersonic'
-]);
-
-angular
-  .module('example')
-  .controller('LearnMoreController', function($scope, supersonic) {
-
-    $scope.navbarTitle = "Learn More";
-
-  });
-
-angular
-  .module('example')
-  .controller('SettingsController', function($scope, supersonic) {
-    $scope.navbarTitle = "Settings";
-  });
-
 function TodoCtrl($scope) {
   
   $scope.todos = [
